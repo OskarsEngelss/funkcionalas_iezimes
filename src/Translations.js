9e13f053bbe7477c2cfbb4be11bf935b7aa43260
Skolnieks <skolnieks.pjvt@pjvt.local>
2025-02-20 08:56:42 +0200

    fixed a bit

--- src/Translations.js
+++ src/Translations.js
@@ -2,14 +2,17 @@ import React from 'react';
 
 function translations() {
     return [
+        { english: 'Feature', latvian: 'Funkcionalitāte' },
+        { english: 'Bakcground', latvian: 'Konteksts' },
+        { english: 'Rule', latvian: 'Rule' },
+        { english: 'Scenario', latvian: 'Scenārijs' },
+        { english: 'Scenario Outline', latvian: 'Scenārija pēc parauga' },
+        { english: 'Examples', latvian: 'Piemēri' },  
         { english: 'Given', latvian: 'Dotais' },
         { english: 'When', latvian: 'Kad' },
         { english: 'Then', latvian: 'Tad' },
         { english: 'And', latvian: 'Un' },
         { english: 'But', latvian: 'Bet' },
-        { english: 'Scenario', latvian: 'Scenārijs' },
-        { english: 'Feature', latvian: 'Funkcionalitāte' },
-        { english: 'Scenario Outline', latvian: 'Scenārija shēma' },
       ];
 }
 
